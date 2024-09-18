70dd4d273e4530aa1e531457672d213c64524e7f
Allenjonesing <Allenjonesing@gmail.com>
2024-09-18 10:10:33 -0500

    lol

--- app.js
+++ app.js
@@ -66,37 +66,37 @@ class BattleScene extends Phaser.Scene {
     console.log("Valid Words Found (Formatted):", this.validWords.length > 0 ? this.validWords.join(', ') : 'No valid words found.');
 
     // Retrieve a word with optional parameters
-    console.log("Retrieved small Word:", getWordByCriteria(this.this.this.this.validWords, {
+    console.log("Retrieved small Word:", getWordByCriteria(this.validWords, {
       minLength: 0,     // Minimum length of 3
       maxLength: 4,     // Maximum length of 4
       preferredStart: null, // Preferred start with 'g'
     }));
-    console.log("Retrieved medium Word:", getWordByCriteria(this.this.this.this.validWords, {
+    console.log("Retrieved medium Word:", getWordByCriteria(this.validWords, {
       minLength: 4,     // Minimum length of 3
       maxLength: 8,     // Maximum length of 4
       preferredStart: null, // Preferred start with 'g'
     }));
-    console.log("Retrieved large Word:", getWordByCriteria(this.this.this.this.validWords, {
+    console.log("Retrieved large Word:", getWordByCriteria(this.validWords, {
       minLength: 8,     // Minimum length of 3
       maxLength: 16,     // Maximum length of 4
       preferredStart: null, // Preferred start with 'g'
     }));
-    console.log("Retrieved 'fire' Word:", getWordByCriteria(this.this.this.this.validWords, {
+    console.log("Retrieved 'fire' Word:", getWordByCriteria(this.validWords, {
       minLength: null,     // Minimum length of 3
       maxLength: null,     // Maximum length of 4
       preferredStart: 'fire', // Preferred start with 'g'
     }));
-    console.log("Retrieved 'ice' Word:", getWordByCriteria(this.this.this.this.validWords, {
+    console.log("Retrieved 'ice' Word:", getWordByCriteria(this.validWords, {
       minLength: null,     // Minimum length of 3
       maxLength: null,     // Maximum length of 4
       preferredStart: 'ice', // Preferred start with 'g'
     }));
-    console.log("Retrieved 'water' Word:", getWordByCriteria(this.this.this.this.validWords, {
+    console.log("Retrieved 'water' Word:", getWordByCriteria(this.validWords, {
       minLength: null,     // Minimum length of 3
       maxLength: null,     // Maximum length of 4
       preferredStart: 'water', // Preferred start with 'g'
     }));
-    console.log("Retrieved 'lightning' Word:", getWordByCriteria(this.this.this.this.validWords, {
+    console.log("Retrieved 'lightning' Word:", getWordByCriteria(this.validWords, {
       minLength: null,     // Minimum length of 3
       maxLength: null,     // Maximum length of 4
       preferredStart: 'lightning', // Preferred start with 'g'
